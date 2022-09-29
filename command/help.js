05f958c54a707122418cea179d13962caf3eb85c
botwhatsapp6 <112824560+botwhatsapp6@users.noreply.github.com>
2022-09-29 10:40:15 -0500

    Update help.js

--- command/help.js
+++ command/help.js
@@ -15,7 +15,7 @@ function toCommas(x) {
 exports.allMenu = (role, ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Yooo sak genla ?🇭🇹
+Yooo sak genla 
 ${mundur}
 
 *STATISTICS*
